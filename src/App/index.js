3ccd7718b94be2f3c92917246455f6364e447744
Fabian Castillo <fabianboc960@hotmail.com>
2023-03-28 21:33:44 -0500

    Custom Hook useLocalStorage

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { AppUI } from './AppUI';
-// import './App.css';
 
-const defaultTodos = [
-  { text: 'Cortar Cebolla', completed: true },
-  { text: 'Tomar el curso de intro a React', completed: false },
-  { text: 'Llorar como la llorona', completed: false },
-  { text: 'LALALALALALA', completed: true }
-]
-
+function useLocalStorage(itemName, initValue) {
+  const localStorageItems = localStorage.getItem(itemName);
+  let parsedItem;
+  if (localStorageItems) {
+    parsedItem = JSON.parse(localStorageItems);
+  }else{
+    localStorage.setItem(itemName, JSON.stringify(initValue))
+    parsedItem = initValue;
+  }
+  const [item, setItem] = React.useState(parsedItem)
+  const saveItems = (newItems) => {
+    const stringifiedItems = JSON.stringify(newItems);
+    localStorage.setItem(itemName, stringifiedItems);
+    setItem(newItems);
+  }
+  return [
+    item,
+    saveItems
+  ];
+}
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  
   const [searchValue, setSearchValue] = React.useState('');
-
   const completedTodos = todos.filter(todo=> todo.completed).length;
   const totalTodos = todos.length;
 
@@ -26,17 +38,19 @@ function App() {
       return todoText.includes(searchText);
     })
   }
+
+  
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo=>todo.text===text);
     const newTodo = [...todos];
     newTodo[todoIndex].completed = !newTodo[todoIndex].completed;
-    setTodos(newTodo);
+    saveTodos(newTodo);
   }
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo=>todo.text===text);
     const newTodo = [...todos];
     newTodo.splice(todoIndex,1);
-    setTodos(newTodo);
+    saveTodos(newTodo);
   }
   return (
     <AppUI
